6e947193abd6a1e99fc357665dd3de09584ba7f4
cmanage1 <55030581+cmanage1@users.noreply.github.com>
2019-10-02 15:10:31 -0400

    Add files via upload

---
+++ Assignment 5/Question 2.c
@@ -0,0 +1,179 @@
+/*-----------------------------------------------------------------
+Author: Chethin Manage
+Date: Oct 20th 2018 (Fall 2018 Semester)
+Course: GNG 1106
+Description: This program calculates the average velocity of water in a
+             in a rectangular open channel with separate functions.
+
+------------------------------------------------------------------*/
+#include <stdio.h>
+#include <math.h>
+#define NUM_VALUES 25 //the number of values that need to displayed
+#define NUM_ROWS 2 //the number of rows in 2D array (depth and velocity)
+#define D_IX 0 //index for depth values
+#define V_IX 1 //index for velocity values
+
+/*--------------structure for holding the variables---------------*/
+typedef struct
+{
+    char name[10]; //name of the channel
+    double n; //roughness coefficient
+    double slope; //value of S
+    double width; //value of B
+    double maxDepth; //maximum water depth possible
+
+} CHANNEL;
+/*----------------------------------------------------------------*/
+
+//function prototypes
+void getInput(CHANNEL *);
+double getPositiveValue();
+void fillArray(CHANNEL *, int n, double[][n]);
+double computeVelocity(CHANNEL *, double);
+double displayTable(CHANNEL *, int n, double *, double *);
+
+/*-----------------------------------------------------------------
+                        MAIN FUNCTION
+Description: Gets all the inputs from the user, checks for positive
+            inputs by calling function "getPositiveValue" and calls the function
+            "DisplayTable" to print the outputs.
+------------------------------------------------------------------*/
+void main()
+{
+    CHANNEL channel; //defining structure as a variable
+
+    //This is a 2 dimensional array
+    //[D_I] contains the depth values
+    //[V_I] contains the velocity values
+    double values[NUM_ROWS][NUM_VALUES];
+
+    getInput(&channel); //calling function to get inputs
+    fillArray(&channel, NUM_VALUES, values); //calling function to fill the 2D array
+    displayTable(&channel, NUM_VALUES, values[D_IX], values[V_IX]); //calls the function to display the table
+}
+
+
+/*---------------------------------------------------------------------------
+                               FUCNTION GET INPUT
+Parameters:
+        *ptr- A pointer to structure CHANNEL
+Description: This function gets the users input and then assigns it to the
+            proper variable in the structure CHANNEL using pointer *ptr
+-----------------------------------------------------------------------------*/
+void getInput(CHANNEL *ptr)
+{
+    printf("Give the name of the channel:"); //asks user input for channel name
+    fgets(ptr->name, 10, stdin); //scans 10 characters the user inputs and stores it in the variable
+
+    printf("\nGive the coefficient of roughness:"); //asks user input for coefficient of roughness
+    ptr->n = getPositiveValue(); //calls function to check for - or 0 values
+
+    printf("\nGive the slope:"); //asks user input for slope
+    ptr->slope = getPositiveValue();
+
+    printf("\nGive the channel width:"); //asks user input for the channel width
+    ptr->width = getPositiveValue();
+
+    printf("\nGive the maximum depth of the channel:"); //asks user input for max depth
+    ptr->maxDepth= getPositiveValue();
+}
+
+/*------------------------------------------------------------------------
+                     POSITIVE VALUE CHECK FUNCTION
+Returns: A positive value that the user input
+Description: This function scans the user input and determines if it is
+            positive. If 0 or negative, the user is asked to enter an input again.
+--------------------------------------------------------------------------*/
+double getPositiveValue()
+{
+    double value; //declares value; the variable inside the function as a double
+    do
+    {
+        scanf("%lf", &value); //scans what the user entered
+        if (value <= 0.0) // if user input is 0 or less. another chance to input is give
+            printf("\nThe value you entered must be greater zero:"); //prompted message to user
+    }while (value <= 0.0); //this loops keeps on going until the user inputs greater than 0
+
+    return value; //returns the input back to main function so it can get assigned to the desired variable
+}
+
+
+/*---------------------------------------------------------------------------
+                        FUNCTION THAT FILLS VALUE ARRAYS
+Parameters:
+        *ptr- A pointer to the structure CHANNEL
+        n- the number of values that need to be calculated
+            (number of columns in the 2D array)
+        mat-a pointer to the 2D array values with n, the number
+            of columns
+Description: This functions fills 2D array values with depth and velocity values
+            calculated using the computeVelocity function
+----------------------------------------------------------------------------*/
+void fillArray(CHANNEL *ptr, int n, double mat[][n])
+{
+    int i;
+    double incr;
+    double depth;
+    CHANNEL channel;
+
+    incr= ptr->maxDepth/(NUM_VALUES); //the increment in which the depth value of the table goes up by
+    depth= incr; //the first value for density gets set to the increment instead of 0 because the function\
+                cannot calculate a value when density is 0
+
+    for (i=0; i<NUM_VALUES; i++)
+    {
+        mat[D_IX][i]= depth; //the first of 'i' is set to the first value of density in the array
+        mat[V_IX][i]= computeVelocity(ptr, depth); //the first value of 'i' gets set to the first value of velocity in the array
+        depth += incr; //the density value goes up by the value of the increment
+    }
+}
+/*------------------------------------------------------------------------------
+                        DISPLAY TABLE FUNCTION
+Parameters:
+        channel - structure with all variables stored
+Description: This function first calculates the values that need to be printed
+            on the table by calling the "computeVelocity" function. Then prints those values
+            on a table.
+-------------------------------------------------------------------------------*/
+double displayTable (CHANNEL *ptr, int n, double *depth, double *velocity)
+{
+    int i; //variable used as a counter for the 'for' loop
+
+    printf("Channel data for '%s'\
+           \n  Coefficient of roughness: %.4lf\
+           \n  Slope: %.5lf\
+           \n  Width: %.2lf\
+           \n  Maximum Depth: %.2lf", ptr->name, \
+           ptr->n, ptr->slope, ptr->width, ptr->maxDepth); //prints what the user input
+
+    printf("\nDepth        Average Velocity\
+          \n-----------------------------");
+           for (i=0; i < NUM_VALUES; i++) //the loop increases by 1 every time until it reaches 25
+           {
+                printf("\n%.2lf               %.4lf", depth[i], velocity[i]); //prints out arrays
+           }
+}
+
+/*------------------------------------------------------------------------
+                            CALCULATION FUNCTION
+Parameters:
+        *ptr- a pointer to the structure CHANNEL
+        d - the incremented depth value that goes into the equation
+Returns:
+        vel- the average velocity at a certain density
+Description: This function calculates the average velocity and returns it to
+        "DisplayTable" where it is being called.
+--------------------------------------------------------------------------*/
+double computeVelocity(CHANNEL *ptr, double d)
+{
+    double vel; //declaring variable for velocity
+    double x, y; //declaring variables to help write down the equation
+
+    //equations to calculate Average Velocity at a certain density
+    vel= (sqrt(ptr->slope))/ptr->n;
+    x= ((ptr->width/d)/(ptr->width+ 2.0*d));
+    y= 2.0/3.0;
+    vel= (pow(x,y) ) * vel;
+
+    return vel;
+}
---
+++ Assignment 5/Question 3.c
@@ -0,0 +1,224 @@
+/*-----------------------------------------------------------------
+Author: Chethin Manage
+Date: Oct 20th 2018 (Fall 2018 Semester)
+Course: GNG 1106
+Description: This program calculates the average velocity of water in a
+             in a rectangular open channel with separate functions.
+------------------------------------------------------------------*/
+#include <stdio.h>
+#include <math.h>
+#define NUM_VALUES 20
+#define NUM_ROWS 2
+#define A_I 0
+#define Y_I 1
+#define TRUE 1
+#define FALSE 0
+
+
+
+/*-----------------------Defining Structure-----------------------*/
+typedef struct
+{
+    char name[10]; //name of the forest
+    double totalAcres; //total number of acres
+    double minAcres; //minimum number of acres uncut
+    double maxAcres; //maximum number of acres uncut
+    double rate; //reforestation rate
+} FOREST;
+
+//function prototypes
+void getForestInput(FOREST *);
+double getPositiveValue();
+void calculateTableData(FOREST *ptr, int n, double *, double *);
+void displayTable(FOREST *ptr, int n, double *, double *);
+
+
+/*-------------------------------------------------------------------
+                            MAIN FUNCTION
+Description: This function calls three functions to get inputs, fill
+            fill the arrays, and print out the table at the end. After
+            these functions, the main function will ask the user if
+            they wish to keep going with the program.
+--------------------------------------------------------------------*/
+void main()
+{
+    int endProgram; //defining a flag to quit the loop and end program
+    endProgram=FALSE;
+
+    do
+    {
+        FOREST forest; //declaring the structure variable
+
+        double data[NUM_VALUES][NUM_VALUES]; //declaring a 2D array
+
+        getForestInput(&forest); //calling function to get input
+        calculateTableData(&forest, NUM_VALUES, data[A_I], data[Y_I]); //calling function to fill the arrays
+        displayTable(&forest, NUM_VALUES, data[A_I], data[Y_I]); //calling function to print the table
+
+        int badInput;  //defining a flag to differentiate a bad input
+        badInput=FALSE;
+
+        do
+        {
+            char input; //declaring the character input variable (y/n)
+
+            printf("\nDo you wish to quit the program(y/n):");
+            scanf("%s", &input); //scanning user input
+            if (input == 'y')
+                {
+                endProgram = TRUE; //flag set TRUE to end program (break out of the loop)
+                badInput=FALSE; //flag set FALSE to break out of the loop
+                printf("\n**PROGRAM TERMINATED**\n");}
+            else if (input == 'n')
+                {
+                fflush(stdin); //clears all the values assigned to variables
+                endProgram = FALSE;
+                badInput=FALSE;
+                printf("\n**This program will now restart**\n\n");}
+            else
+                {
+                printf("\nPlease enter a valid input!\n");
+                badInput=TRUE;}
+        }while (badInput==TRUE); //loop while keep going when flag is set TRUE
+
+    }while (endProgram == FALSE); //loop will keep going when flag is set FALSE
+
+}
+
+
+/*----------------------------------------------------------------------
+                            FUNCTION TO GET INPUT
+Parameters:
+            FOREST *ptr- a pointer to the structure FOREST. Used to assign
+            input values into the structure.
+Description: This function will ask the user to input values and stores them
+            in the structure FOREST. The function will also call function
+            getPositiveValues in order to check for negatives. The function
+            will also check if the minAcres is less than the maxAcres.
+----------------------------------------------------------------------*/
+void getForestInput(FOREST *ptr)
+{
+    int flag; //declaring a flag so that loop keeps going at bad inputs
+    flag=FALSE;
+
+    printf("Forest name: "); //gets the name of the forest from user
+    fgets(ptr->name, 10, stdin); //this functions uses a pointer to store input in structure
+
+    do{
+    if (flag== TRUE) //this will prompt the user to enter their values again when the loop starts again
+        printf("\nPlease enter all the values again\n\n");
+
+    printf("Total acres: "); //gets the total acres from user
+    ptr->totalAcres=getPositiveValue(); //checks if value is greater than or equal to 0
+
+    printf("Areas Uncut Minimum: ");
+    ptr->minAcres=getPositiveValue();
+
+    printf("Areas Uncut Maximum: ");
+    ptr->maxAcres=getPositiveValue();
+        if (ptr->minAcres >= ptr->maxAcres) //makes sure minimum is less than max
+        {
+            printf("\n**The Areas Uncut Minimum has to be less than Areas Uncut Maximum!**");
+            flag = TRUE; //flag set true to keep the loop going
+        }
+        else if (ptr->totalAcres <= ptr->maxAcres) //makes sure total is more than max
+        {
+            printf("\n**The Maximum Area Uncut is greater than the Total Area!**");
+            flag = TRUE; //flag set true to keep the loop going
+        }
+        else
+            flag =FALSE; //flag turns to FALSE if no bad inputs are found (breaks out of the loop)
+    }while (flag == TRUE);
+
+    //flag stays false from the last loop
+    //this loop is to  make sure rate stays between 0 and 1
+    do
+    {
+        printf("Reforestation rate: ");
+        ptr->rate=getPositiveValue();
+        if (ptr->rate >= 1.0 || ptr->rate < 0.0)
+        {
+            printf("\nThe rate must be between 0 and 1!\n");
+            flag=TRUE; //sets flag to TRUE so the loop keeps going
+        }
+        else
+            flag=FALSE; //sets flag to FALSE
+    }while (flag == TRUE); //breaks out of loop of flag is FALSE
+
+}
+
+
+/*-----------------------------------------------------------------------
+                    FUNCTION TO CHECK POSITIVE VALUES
+Returns:
+        value - a double value which is the user input
+Description: Checks if the user input is greater than or equal to 0 and
+            returns "value" to main function.
+------------------------------------------------------------------------*/
+double getPositiveValue()
+{
+    double value; //declares variable that the function will return
+    do
+    {
+        scanf("%lf", &value);
+        if (value <= 0.0)
+            printf("\nPlease enter a value above zero:");
+    }while (value <= 0.0 ); //loop keeps going while the user input is less than or equal to 0
+    return value; //function return
+}
+
+/*------------------------------------------------------------------------
+                    FUNCTION TO FILL IN TABLE DATA
+Parameters:
+        FOREST *ptr- A pointer used to access the values in the structure FOREST
+        n- an integer with the number of values (used instead of using symbolic constants)
+        *area- a pointer used to access values in the first part of the 2D array
+        *years- a pointer used to access values in the second part of the 2D array
+Description: This function fills in the arrays using a nested loop
+-------------------------------------------------------------------------*/
+void calculateTableData(FOREST *ptr, int n, double *area, double *years)
+{
+    int i; //declaring variable used in the foor
+    double unCutA=ptr->minAcres; //declaring and making the first value of the array equal to min acres
+    double reforest; //declaring a value used for area that is reforested
+    double incr= (ptr->maxAcres - ptr->minAcres)/(n-1); //increment that the first column goes up by
+
+    for (i=0; i< n; i++) //the loop counter goes up untilmax number of values
+    {
+        years[i]=0; //the number of years to reforest gets set to 0
+        area[i]=unCutA; //the first area value is set equal to the min areas uncut
+        reforest=unCutA; //sets the reforest
+        do
+        {
+            reforest+= ((ptr->rate)*(reforest)); //the area reforested gets added to the previous value of it multiplied by the rate
+            years[i] += 1; //the number of years goes up by 1 every time while loop repeats
+        }while (reforest < ptr->totalAcres); // loop repeats while area reforested is less than total area
+        unCutA+= incr; //the uncut area goes up by the increment every time for loop repeats
+    }
+}
+
+/*------------------------------------------------------------------------
+                    FUNCTION TO DISPLAY TABLE
+Parameters:
+        FOREST *ptr- A pointer used to access the values in the structure FOREST
+        n- an integer with the number of values (used instead of using symbolic constants)
+        *area- a pointer used to access values in the first part of the 2D array
+        *years- a pointer used to access values in the second part of the 2D array
+Description: This function prints out the with all calculated data
+-------------------------------------------------------------------------*/
+void displayTable(FOREST *ptr, int n, double *area, double *years)
+{
+    int i; //an integer to be used in the for loop
+
+    printf("Forest: %s\
+           \n     Uncut Area: min = %.2lf, max = %.2lf\
+           \n     Reforestation Rate: %.3lf\n"\
+           , ptr->name, ptr->minAcres, ptr->maxAcres, ptr->rate); //printing all user inputs
+
+    printf("\nUncut Area Start           Num Years For Total Reforestation");
+    for (i=0; i < n; i++) //for loop used to access separate values in the arrays
+    {
+        printf("\n      %.2lf                     %.0lf", area[i], years[i]); //prints out the arrays
+    }
+
+}
